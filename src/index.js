af8170cba986826d0daf6f36c916710740026d67
zhaohongbo <zhaohongbo8@gmail.com>
2018-03-09 22:16:23 +0800

    remove register worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './redux/reducers';
 import RootRouter from './router/rootRouter';
-import registerServiceWorker from './registerServiceWorker';
 import 'antd/dist/antd.css'
 import './index.css';
 
@@ -16,4 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
